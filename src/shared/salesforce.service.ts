64465ac50b1ba4158dae3b9d2b84f15c03725065
Dustin E. Homan <dustin.e.homan@gmail.com>
2017-08-25 13:24:38 -0600

    Omit fields are now a const

--- src/shared/salesforce.service.ts
+++ src/shared/salesforce.service.ts
@@ -3,41 +3,61 @@ import * as jsforce from 'jsforce';
 import * as deepClean from 'deep-cleaner';
 
 export interface QueryRequest {
-    action : string,
-    fields : string[],
-    table : string,
-    clauses? : string
+    action: string,
+    fields: string[],
+    table: string,
+    clauses?: string
 }
 
 export interface IdRequest {
-    object : string,
-    ids : string[]
+    object: string,
+    ids: string[]
 }
 
 export interface JSONObject {
-    contents : string
+    contents: string
 }
 
 export interface RecordsRequest {
-    object : string,
+    object: string,
     records: JSONObject[]
 }
 
 export interface SearchRequest {
-    search : string,
-    retrieve : string
+    search: string,
+    retrieve: string
 }
 
-export class SalesforceService {
-    static conn = new jsforce.Connection({loginUrl: process.env.SF_URL, instanceURL: process.env.SF_ENV});
+const OMIT_FIELDS = ['LastModifiedDate',
+    'IsDeleted',
+    'LastViewedDate',
+    'LastReferencedDate',
+    'SystemModstamp',
+    'CreatedById',
+    'CreatedDate',
+    'LastModifiedById',
+    'JigsawCompanyId',
+    'PhotoUrl',
+    'MasterRecordId',
+    'IsEmailBounced',
+    'OtherAddress',
+    'LastCUUpdateDate',
+    'Contact_Quality__c',
+    'MailingAddress',
+    'LastCURequestDate',
+    'LastActivityDate',
+    'JigsawContactId']
 
-    static async query(queryRequest : QueryRequest) {
+export class SalesforceService {
+    static conn = new jsforce.Connection({ loginUrl: process.env.SF_URL, instanceURL: process.env.SF_ENV });
+
+    static async query(queryRequest: QueryRequest) {
         // this.conn = bluebird.promisifyAll(this.conn);
         let queryString = queryRequest.action;
-        queryRequest.fields.forEach((f,i)=>{ queryString += " " + f + (i < queryRequest.fields.length - 1 ? "," : "") });
+        queryRequest.fields.forEach((f, i) => { queryString += " " + f + (i < queryRequest.fields.length - 1 ? "," : "") });
 
         queryString += " FROM " + queryRequest.table;
-        if(queryRequest.clauses) queryString += " WHERE " + queryRequest.clauses;
+        if (queryRequest.clauses) queryString += " WHERE " + queryRequest.clauses;
 
         console.log('DEBUG: Executing SOQL: ', queryString);
         try {
@@ -51,118 +71,100 @@ export class SalesforceService {
             SalesforceService.conn.logout();
             return Promise.resolve({ contents: JSON.stringify(res) });
 
-        } catch(error) {
+        } catch (error) {
             console.error('Error in SalesforceService.query(): ', error);
             return Promise.reject(error);
         }
     }
 
-    static async retrieve(idRequest : IdRequest) {
-        try{
+    static async retrieve(idRequest: IdRequest) {
+        try {
             // Login to Salesforce
             await SalesforceService.conn.login(process.env.SF_USER, process.env.SF_PASS);
             // Retrieve records for given object and ids
             let res = await SalesforceService.conn.sobject(idRequest.object).retrieve(idRequest.ids);
             deepClean(res, 'attributes');
             SalesforceService.conn.logout();
-            return Promise.resolve({contents: JSON.stringify(res)});
-        } catch(error) {
+            return Promise.resolve({ contents: JSON.stringify(res) });
+        } catch (error) {
             return Promise.reject(error);
         }
     }
 
-    static async create(recordsRequest : RecordsRequest) {
+    static async create(recordsRequest: RecordsRequest) {
         let records = new Array();
         recordsRequest.records.forEach(record => {
-            let newRecord = _.omit(JSON.parse(record.contents), [
-                'LastModifiedDate',
-                'IsDeleted',
-                'LastViewedDate',
-                'LastReferencedDate',
-                'SystemModstamp',
-                'CreatedById',
-                'CreatedDate',
-                'LastModifiedById'
-            ]);
+            let newRecord = _.omit(JSON.parse(record.contents), OMIT_FIELDS);
             records.push(newRecord)
         });
-        try{
+        try {
             await SalesforceService.conn.login(process.env.SF_USER, process.env.SF_PASS);
             let res = await SalesforceService.conn.sobject(recordsRequest.object).create(records);
             SalesforceService.conn.logout();
-            return Promise.resolve({contents: JSON.stringify(res)});
-        } catch(error) {
+            return Promise.resolve({ contents: JSON.stringify(res) });
+        } catch (error) {
             console.log('Error in SalesforceService.create(): ', error);
             return Promise.reject(error);
         }
     }
 
-    static async update(recordsRequest : RecordsRequest) {
+    static async update(recordsRequest: RecordsRequest) {
         let records = new Array();
         recordsRequest.records.forEach(record => {
-            let newRecord = _.omit(JSON.parse(record.contents), [
-                'LastModifiedDate',
-                'IsDeleted',
-                'LastViewedDate',
-                'LastReferencedDate',
-                'SystemModstamp',
-                'CreatedById',
-                'CreatedDate',
-                'LastModifiedById'
-            ]);
+            let newRecord = _.omit(JSON.parse(record.contents), OMIT_FIELDS);
             records.push(newRecord)
         });
         try {
             await SalesforceService.conn.login(process.env.SF_USER, process.env.SF_PASS);
             let res = await SalesforceService.conn.sobject(recordsRequest.object).update(records);
             SalesforceService.conn.logout();
-            return Promise.resolve({contents: JSON.stringify(res)});
-        } catch(error) {
+            return Promise.resolve({ contents: JSON.stringify(res) });
+        } catch (error) {
             return Promise.reject(error);
         }
     }
 
-    static async delete(idRequest : IdRequest) {
+    static async delete(idRequest: IdRequest) {
         try {
             await SalesforceService.conn.login(process.env.SF_USER, process.env.SF_PASS);
             let res = await SalesforceService.conn.sobject(idRequest.object).delete(idRequest.ids);
             SalesforceService.conn.logout();
-            return Promise.resolve({contents: JSON.stringify(res)});
-        } catch(error) {
+            return Promise.resolve({ contents: JSON.stringify(res) });
+        } catch (error) {
             return Promise.reject(error);
         }
     }
 
-    static async upsert(recordsRequest : RecordsRequest) {
-        try{
+    static async upsert(recordsRequest: RecordsRequest) {
+        try {
             await SalesforceService.conn.login(process.env.SF_USER, process.env.SF_PASS)
             let res = await SalesforceService.conn.sobject(recordsRequest.object).upsert(recordsRequest.records);
             SalesforceService.conn.logout();
-            return Promise.resolve({contents: JSON.stringify(res)});
-        } catch(error) {
+            return Promise.resolve({ contents: JSON.stringify(res) });
+        } catch (error) {
             return Promise.reject(error);
         }
     }
 
-    static async describe(object : string){
-        try{
-            await SalesforceService.conn.login(process.env.SF_USER, process.env.SF_PASS); 
+    static async describe(object: string) {
+        try {
+            await SalesforceService.conn.login(process.env.SF_USER, process.env.SF_PASS);
             let res = await SalesforceService.conn.sobject(object).describe();
             SalesforceService.conn.logout();
-            return Promise.resolve({contents: JSON.stringify(res)});
-        } catch(error) {
+            return Promise.resolve({ contents: JSON.stringify(res) });
+        } catch (error) {
             return Promise.reject(error);
         }
     }
 
-    static async search(searchRequest : SearchRequest) {
-        try{
+    static async search(searchRequest: SearchRequest) {
+        try {
             await SalesforceService.conn.login(process.env.SF_USER, process.env.SF_PASS);
             let res = await SalesforceService.conn.search(`FIND ${searchRequest.search} IN ALL FIELDS RETURNING ${searchRequest.retrieve}`);
             deepClean(res, 'attributes');
             SalesforceService.conn.logout();
-            return Promise.resolve({contents: JSON.stringify(res)});
-        } catch(error) {
+            return Promise.resolve({ contents: JSON.stringify(res) });
+        } catch (error) {
             return Promise.reject(error);
         }
     }
