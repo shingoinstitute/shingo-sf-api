b34513fea097817271a6b73045c88479afcbb216
Abraham White <abelincoln.white@gmail.com>
2018-06-05 16:13:15 -0600

    Fix run script

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 TAG="${TAG:-latest}"
 NETWORK="${NETWORK:-shingo-net}"
 PORT="${PORT:-80}"
+LOG_PATH="${LOG_PATH:-/logs}"
 
 if [ -z "$SF_USER" ]; then
     echo "Need the SF_USER env variable to be defined..."
@@ -24,9 +25,9 @@ if [ -z "$SF_URL" ]; then
     exit 1
 fi
 
-docker run -itd           \
-    --name shingo-sf-api  \
-    --network "$NETWORK"  \
-    -e LOG_PATH="/logs"   \
-    -v /logs:/logs:rw     \
+docker run -itd             \
+    --name shingo-sf-api    \
+    --network "$NETWORK"    \
+    -e LOG_PATH="/logs"     \
+    -v "$LOG_PATH":/logs:rw \
     docker.shingo.org/shingo-sf-api"$TAG"
