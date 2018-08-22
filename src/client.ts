b6b0cad80a71d1cc90c954506f8332fbf15d673c
Abraham White <abelincoln.white@gmail.com>
2018-08-22 14:42:16 -0600

    Refine build process and rename client class

--- src/client.ts
+++ src/client.ts
@@ -15,10 +15,10 @@ const throwOnUndefined = <T>(x: T | undefined): T => {
   return x
 }
 
-export class SalesforceService {
+export class SalesforceClient {
   client: ServiceClient
 
-  constructor(address: string) {
+  constructor(address: string, creds?: grpc.ChannelCredentials) {
     const protoFile = path.join(__dirname, './proto', 'sf_services.proto')
     const options: ProtoOptions = {
       keepCase: true,
@@ -30,51 +30,83 @@ export class SalesforceService {
     const packageDefinition = loadSync(protoFile, options)
     const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).sfservices as grpc.GrpcObject
     const clientClass = protoDescriptor.SalesforceMicroservice as typeof grpc.Client
-    this.client = bindAll(new clientClass(address, grpc.credentials.createInsecure()) as ServiceClient)
+    this.client = bindAll(new clientClass(address, creds || grpc.credentials.createInsecure()) as ServiceClient)
   }
 
+  /**
+   * Query salesforce fields using SOQL SELECT
+   * @param req QueryRequest object
+   */
   query<T>(req: QueryRequest): Promise<QueryResponse<T>> {
     return promisify(this.client.query)(req)
       .then(throwOnUndefined)
       .then(unjsonobject)
   }
 
+  /**
+   * Retrieve salesforce objects by id
+   * @param req IdRequest object
+   */
   retrieve(req: IdRequest): Promise<object> {
     return promisify(this.client.retrieve)(req)
       .then(throwOnUndefined)
       .then(unjsonobject)
   }
 
+  /**
+   * Create a salesforce object
+   * @param req RecordsRequest object
+   */
   create(req: RecordsRequest): Promise<RecordResult[]> {
     return promisify(this.client.create)(req)
       .then(throwOnUndefined)
       .then(unjsonobject)
   }
 
+  /**
+   * Update a salesforce object
+   * @param req RecordsRequest object
+   */
   update(req: RecordsRequest): Promise<RecordResult[]> {
     return promisify(this.client.update)(req)
       .then(throwOnUndefined)
       .then(unjsonobject)
   }
 
+  /**
+   * Delete salesforce objects by id
+   * @param req IdRequest object
+   */
   delete(req: IdRequest): Promise<RecordResult[]> {
     return promisify(this.client.delete)(req)
       .then(throwOnUndefined)
       .then(unjsonobject)
   }
 
+  /**
+   * Upsert salesforce objects
+   * @param req UpsertRequest object
+   */
   upsert(req: UpsertRequest): Promise<RecordResult[]> {
     return promisify(this.client.upsert)(req)
       .then(throwOnUndefined)
       .then(unjsonobject)
   }
 
+  /**
+   * Describe salesforce object
+   * @param object Salesforce object to describe
+   */
   describe(object: string): Promise<DescribeSObjectResult> {
     return promisify(this.client.describe)({ object })
       .then(throwOnUndefined)
       .then(unjsonobject)
   }
 
+  /**
+   * Execute SOSL search
+   * @param req SearchRequest object
+   */
   search(req: SearchRequest): Promise<{ searchRecords: any[] }> {
     return promisify(this.client.search)(req)
       .then(throwOnUndefined)
