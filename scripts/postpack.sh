9f5053fe3b2f2a69a52610da5947ec797536d502
Abraham White <abelincoln.white@gmail.com>
2018-08-23 12:27:51 -0600

    Update postpack script

--- scripts/postpack.sh
+++ scripts/postpack.sh
@@ -3,6 +3,6 @@
 # Cleans up emitted build files and directories
 
 # if any paths are relative to the build directory, make them relative to root
-awk '{gsub("'"$PWD/build"'", "'"$PWD"'"); printf("%s%c", $0, 0)}' ./*.emit | xargs -0 rm -rf
+awk '{gsub("'"$PWD/build/"'", "'"$PWD/"'"); printf("%s%c", $0, 0)}' ./*.emit | xargs -0 rm -rf
 
 rm ./*.emit
